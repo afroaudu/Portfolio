3266a0a8a2fb1bb8946192f6ab3da01e518ef86d
unknown <caleb.audu@primaboost.io>
2022-06-03 08:13:55 +0100

    added a picture to yhr page and also added css animations to make it look better

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -1,5 +1,5 @@
 import "../introduction/Intro.css"
-import Kelly from "../../images/young.webp"
+import Kelly from "../../images/dorrells.png"
 
 
 const Introduction = () => {
@@ -7,20 +7,38 @@ const Introduction = () => {
       <div className='i'>
         <div className='i-left'>
           <div className='i-left-wrapper'>
-            <h1>Hello, I am Kelly Audu</h1>
-            <h5>
-              AWS Backend Software Engineer || Software Consultant
-              <h5>with over 6 years of industry experience</h5>
-            </h5>
+            <h1 className='i-intro'>Hello, I am Kelly Audu</h1>
+            <div className='i-title'>
+              <div className='i-title-wrapper'>
+                <div className='i-title-item'>AWS EXPERT</div>
+                <div className='i-title-item'>SOFTWARE CONSULTANT</div>
+                <div className='i-title-item'>FULL-STACK ENGINEER</div>
+                <div className='i-title-item'>WRITER</div>
+                <div className='i-title-item'>FRONT-END DEVELOPER</div>
+              </div>
+            </div>
+            <div className='i-desc'>
+              I provide the necessary expertise to help businesses make
+              important decisions about their digital technology and online
+              presence. Since software choices often impact ecommerce, let me help your
+              company protect their investments and
+              increase sales.
+            </div>
             <div className='i-left-wrapper-buttons'>
-              <button className='i-left-wrapper-buttons-left'> Contact Us</button>
-              <button className="i-left-wrapper-buttons-right"> Download Resume</button>
+              <button className='i-left-wrapper-buttons-left'>
+                {' '}
+                Contact Us
+              </button>
+              <button className='i-left-wrapper-buttons-right'>
+                {' '}
+                Download Resume
+              </button>
             </div>
           </div>
         </div>
-            <div className='i-right'>
-                <img src={Kelly} alt="" className="i-img" />
-            </div>
+        <div className='i-right'>
+          <img src={Kelly} alt='' className='i-img' />
+        </div>
       </div>
     );
 };
